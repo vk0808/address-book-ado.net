aedc55d68b1690accb50717234882b8d4687fd48
vk0808 <vk4765712@gmail.com>
2021-09-09 20:57:26 +0530

    feat: retrieve from db

---
+++ AddressBook/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Welcome message
+            Console.WriteLine("Address Book ADO.NET\n");
+
+            // Object instantiation
+            PersonModel person = new PersonModel();
+            AddressBookRepo repo = new AddressBookRepo(); 
+
+            // Call method
+            repo.GetAllPeople();
+        }
+    }
+}
